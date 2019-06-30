304e157674a0a7bc0b8066f6213e287e603a0088
Nemanja Cindric <cindrano1@gmail.com>
2019-06-30 11:35:10 +0200

    Added base models and rendering for companies.

---
+++ Web/Models/Filtering/CompanyFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeshEAT.Web.Interfaces;
+using TimeshEAT.Web.Models.Render.Company;
+
+namespace TimeshEAT.Web.Models.Filtering
+{
+    public class CompanyFilter : IFilter<CompanyDetailsRenderModel>
+    {
+        public string Letter { get; set; }
+        public string Query { get; set; }
+
+        public IReadOnlyList<CompanyDetailsRenderModel> Apply(IReadOnlyList<CompanyDetailsRenderModel> items) =>
+            items.Where(x => LetterFiltering(x) && QueryFiltering(x))
+                .ToList()
+                .AsReadOnly();
+
+        private bool LetterFiltering(CompanyDetailsRenderModel user) => true;
+        private bool QueryFiltering(CompanyDetailsRenderModel user) => true;
+    }
+}
---
+++ Web/Models/Pagination/CompanyPagedCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeshEAT.Business.Models;
+using TimeshEAT.Web.Models.Filtering;
+using TimeshEAT.Web.Models.Render;
+using TimeshEAT.Web.Models.Render.Company;
+
+namespace TimeshEAT.Web.Models.Pagination
+{
+    public class CompanyPagedCollection : ReadOnlyPagedCollection<CompanyDetailsRenderModel>
+    {
+        private readonly Lazy<IList<MealModel>> meals;
+        public List<SelectListItem> MealList => new List<SelectListItem>(meals.Value
+            .Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }));
+
+        public CompanyPagedCollection(IReadOnlyList<CompanyDetailsRenderModel> items, int page, int itemsPerPage, CompanyFilter filter = null) : base(filter?.Apply(items) ?? items, page, itemsPerPage)
+        {
+            meals = new Lazy<IList<MealModel>>(() => _api.GetAllMeals<MealModel>().Data);
+
+            foreach (CompanyDetailsRenderModel company in Items)
+            {
+                company.MealList = MealList;
+            }
+        }
+    }
+}
