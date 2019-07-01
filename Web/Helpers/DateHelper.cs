e6957ff9c91d8d86de96a9ab8b241629305fae36
mbogunovic <m.bogunovic@vegaitsourcing.rs>
2019-07-01 22:09:41 +0200

    meals month table update

---
+++ Web/Helpers/DateHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TimeshEAT.Web.Helpers
+{
+	public class DateHelper
+	{
+		public static DateTime GetMonthEndDate(DateTime date)
+		{
+			DateTime endDate = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+			for (; endDate.DayOfWeek != DayOfWeek.Sunday;)
+			{
+				endDate = endDate.AddDays(1);
+			}
+			return endDate;
+		}
+
+		public static DateTime GetMonthStartDate(DateTime date)
+		{
+			DateTime startDate = new DateTime(date.Year, date.Month, 1);
+			for (; startDate.DayOfWeek != DayOfWeek.Monday;)
+			{
+				startDate = startDate.AddDays(-1);
+			}
+			return startDate;
+		}
+
+		public static DateTime GetWeekEndDate(DateTime date)
+		{
+			DateTime end = date;
+			for (; end.DayOfWeek != DayOfWeek.Sunday;)
+			{
+				end = end.AddDays(1);
+			}
+			return end;
+		}
+
+		public static DateTime GetWeekStartDate(DateTime date)
+		{
+			DateTime start = date;
+			for (; start.DayOfWeek != DayOfWeek.Monday;)
+			{
+				start = start.AddDays(-1);
+			}
+			return start;
+		}
+	}
+}
--- Web/Models/View/OrderViewModel.cs
+++ Web/Models/View/OrderViewModel.cs
@@ -1,8 +1,63 @@
-﻿namespace TimeshEAT.Web.Models.View
+﻿using System;
+using System.Collections.Generic;
+using TimeshEAT.Web.Helpers;
+
+namespace TimeshEAT.Web.Models.View
 {
 	public class OrderViewModel : NavigationViewModel
 	{
+		public OrderViewModel()
+		{
+			Days = new Lazy<IEnumerable<DayModel>>(() => GetDays());
+		}
+
 		public override string PageTitle => "Naruči obrok";
 		public override string PageIcon => "home";
+
+		public Lazy<IEnumerable<DayModel>> Days { get; }
+		public DateTime Date { get; set; } = DateTime.Now;
+		public int Total { get; private set; }
+
+		private IEnumerable<DayModel> GetDays()
+		{
+			DateTime startDate = DateHelper.GetMonthStartDate(Date);
+			DateTime endDate = DateHelper.GetMonthEndDate(Date);
+
+			while (startDate <= endDate)
+			{
+				//_api.GetAllMeals()
+
+				//foreach (var activity in _activityService.GetEmployeeActivityForDate(new DailyActivityGetByDate
+				//{
+				//	Date = startDate,
+				//	EmployeeId = model.CurrentUser.Id
+				//}))
+				//{
+				//	total += activity.HoursSpent + activity.Overtime;
+				//}
+
+				var dateModel = new DayModel(startDate, Date);
+				
+				Total += Total;
+				startDate = startDate.AddDays(1);
+
+				yield return dateModel;
+			}
+		}
+
+	}
+
+	public class DayModel
+	{
+		public DayModel(DateTime date, DateTime queryDate)
+		{
+			Date = date;
+			IsActive = Date.ToString("dd.MM.yyyy").Equals(queryDate.ToString("dd.MM.yyyy"));
+			IsDisabled = !Date.ToString("MM.yyyy").Equals(queryDate.ToString("MM.yyyy"));
+		}
+
+		public DateTime Date { get; }
+		public bool IsActive { get; }
+		public bool IsDisabled { get; }
 	}
 }
