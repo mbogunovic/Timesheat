1d28ca4d9d17e618edb72f75ffd8f884235a3be3
mbogunovic <m.bogunovic@vegaitsourcing.rs>
2019-07-01 19:56:43 +0200

    Meals implemented, icons update

---
+++ Web/Controllers/MealController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Mvc;
+using TimeshEAT.Web.Attributes;
+using TimeshEAT.Web.Interfaces;
+using TimeshEAT.Web.Models.Filtering;
+using TimeshEAT.Web.Models.Render;
+using TimeshEAT.Web.Models.View;
+
+namespace TimeshEAT.Web.Controllers
+{
+	[RoleAuthorize(Roles = "Administrator")]
+	public class MealController : BaseController, INavigationController
+	{
+		public ActionResult Index(int page = 1, string letter = null, string query = null)
+		{
+			var model = Navigation.GetPageViewModel<MealViewModel>();
+
+			model.Page = page;
+			model.Filter = new MealFilter(letter, query);
+
+			return View(model);
+		}
+
+
+		[ValidateAntiForgeryToken]
+		[HttpPost]
+		public ActionResult Save(MealDetailsRenderModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return RedirectToAction("Index");
+			}
+
+			if (model.Id == 0)
+			{
+				Business.API.Models.ApiResponseModel<MealDetailsRenderModel> result = _api.AddMeal<MealDetailsRenderModel>(model);
+			}
+			else
+			{
+				_api.UpdateMeal<MealDetailsRenderModel>(model);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		[ValidateAntiForgeryToken]
+		[HttpPost]
+		public ActionResult Delete(MealDetailsRenderModel model)
+		{
+			if (model == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			_api.DeleteMeal(model);
+
+			return RedirectToAction("Index");
+		}
+	}
+}
--- Web/Extensions/IFormExtensions.cs
+++ Web/Extensions/IFormExtensions.cs
@@ -9,6 +9,8 @@ namespace TimeshEAT.Web.Extensions
 	public static class IFormExtensions
 	{
 		public static string DisplayName<T>(this T objModel, Expression<Func<T, object>> propLambda) where T : IForm =>
-			(propLambda.Body as MemberExpression)?.Member?.GetCustomAttribute<DisplayAttribute>()?.Name;
+			(propLambda.Body as MemberExpression)?.Member?
+				.GetCustomAttribute<DisplayAttribute>()?.Name?
+					.Replace(":","");
 	}
 }
