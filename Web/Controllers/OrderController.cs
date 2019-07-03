8b7fd456a5673a647adc6a36c4fafc46ec6fa06e
Maksim Bogunović <m.bogunovic@vegaitsorucing.rs>
2019-07-03 17:00:56 +0200

    implemented order table CRUDE and static

--- Web/Controllers/OrderController.cs
+++ Web/Controllers/OrderController.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
+using TimeshEAT.Business.API;
+using TimeshEAT.Business.Models;
+using TimeshEAT.Domain.Models;
 using TimeshEAT.Web.Attributes;
 using TimeshEAT.Web.Interfaces;
+using TimeshEAT.Web.Membership;
 using TimeshEAT.Web.Models.View;
 
 namespace TimeshEAT.Web.Controllers
@@ -9,12 +15,37 @@ namespace TimeshEAT.Web.Controllers
 	[RoleAuthorize(Roles = "User, Administrator")]
 	public class OrderController : BaseController, INavigationController
     {
-		public ActionResult Index(Constants.Months month)
+		public ActionResult Index(Constants.Months? month = null, DateTime? date = null)
 		{
-			var model = this.Navigation.GetPageViewModel<OrderViewModel>();
-			model.Date = new DateTime(DateTime.Now.Year, (int) month, 1);
+			if (date == null)
+			{
+				var model = this.Navigation.GetPageViewModel<OrderViewModel>();
+				model.Date = new DateTime(DateTime.Now.Year, month != null ? (int)month : DateTime.Now.Month, 1);
 
-			return View(model);
-        }
+				return View(model);
+			}
+			else
+			{
+				var model = new DayOrderRenderModel(date.Value);
+
+				return View("DayOrder", model);
+			}
+		}
     }
+
+	public class DayOrderRenderModel : NavigationViewModel
+	{
+		public override string PageTitle => "Naruči obrok";
+		public override string PageIcon => "home";
+
+		public DayOrderRenderModel(DateTime date)
+		{
+			Orders = new Lazy<IEnumerable<OrderModel>>(() => DependencyResolver.Current.GetService<IApiClient>()
+				.GetAllOrdersBy<OrderModel>((HttpContext.Current.User as MemberPrincipal).Id, date)?.Data);
+			Date = date;
+		}
+
+		public DateTime Date { get; }
+		public Lazy<IEnumerable<OrderModel>> Orders { get; }
+	}
 }
